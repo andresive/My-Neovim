be0d40f90179ef651203ff3159b79ac2d5525a4f
Andresive <gabrielgonzagga40@gmail.com>
2025-10-22 03:52:10 -0300

    update: set discord plugin on debug mode

--- lua/utils.lua
+++ lua/utils.lua
@@ -244,6 +244,7 @@ vim.lsp.enable('omnisharp')
 -- the presence discord connetion plugin Setup 
 require("presence").setup({
     -- General options
+    log_level = "debug",
     auto_update         = true,                       -- Update activity based on autocmd events (if `false`, map or manually execute `:lua package.loaded.presence:update()`)
     neovim_image_text   = "The One True Text Editor", -- Text displayed when hovered over the Neovim image
     main_image          = "neovim",                   -- Main image display (either "neovim" or "file")
